0c3505aea1abe79ef5c0af5cd21efa7878a3e40e
ABhakuni <anurag.bhakuni@gmail.com>
2020-04-27 06:35:53 +0530

    coronawarriors

---
+++ coronawarriors.sh
@@ -0,0 +1,43 @@
+clear
+pyfiglet -j center -w 220 --color=CYAN "#mywaytosalutecoronawarriors"
+echo "\n"
+sleep 2
+pyfiglet -j center -w 220 --color=GREEN "Hi! This is a small appreciation post"
+sleep 1
+pyfiglet -j center -w 220 --color=GREEN "for all the..."
+sleep 1
+pyfiglet -j center -w 220 --color=GREEN "Doctors"
+sleep 1
+pyfiglet -j center -w 220 --color=GREEN "Policemen"
+sleep 1
+pyfiglet -j center -w 220 --color=GREEN "Delivery Boys"
+sleep 1
+pyfiglet -j center -w 220 --color=GREEN "Firefighters"
+sleep 1
+pyfiglet -j center -w 220 --color=GREEN "Store Clerks"
+sleep 1
+pyfiglet -j center -w 220 --color=GREEN "Sanitation Workers"
+echo "\n"
+sleep 2
+pyfiglet -j center -w 220 --color=YELLOW "You all are the real heroes of the world"
+sleep 1
+gif-for-cli -l 1 https://tenor.com/view/thanks-emoji-thumbs-up-gif-10644993
+sleep 1
+clear
+pyfiglet -j center -w 220 --color=RED "Let us also contribute our part"
+sleep 2
+pyfiglet -j center -w 220 --color=RED "to make their job easier..."
+echo "\n\n\n\n\n"
+sleep 3
+pyfiglet -j center -w 220 --color=GREEN "By Staying Safe, Staying Home"
+sleep 3
+pyfiglet -j center -w 220 --color=GREEN "And Taking Care of the People Around Us"
+echo "\n\n\n\n\n"
+sleep 5
+pyfiglet -j center -w 220 --color=CYAN "#mywaytosalutecoronawarriors"
+echo "\n"
+sleep 3
+pyfiglet -j center -w 220 --color=MAGENTA "Use the above hashtag on social media"
+sleep 1
+pyfiglet -j center -w 220 --color=MAGENTA "To salute them"
+sleep 5
